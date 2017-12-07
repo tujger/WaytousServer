541c0f9ab9a68cd6eb92a78ee191dd5d2d521ddb
Tujger <tujger@gmail.com>
2017-12-06 22:43:02 -0500

    WTU-326 Dockerize

--- docker/tasks.sh
+++ docker/tasks.sh
@@ -11,6 +11,10 @@ case $i in
     EMPTY=false
     RUN=true
     ;;
+    -d|--run-detached)
+    EMPTY=false
+    DETACHED=true
+    ;;
     -c|--console)
     EMPTY=false
     CONSOLE=true
@@ -46,15 +50,29 @@ esac
 done
 
 if [ $EMPTY == true ]; then
-    echo tasks.sh -b -r
+    echo tasks.sh -v=VERSION -s -rc -ri -b -r -d -c -l
+    echo All keys are optional, see details
     echo    -b, --build - build/rebuild image
     echo    -r, --run - run container
+    echo    -d, --run-detached - run container detached
     echo    -s, --stop - stop container
     echo    -l, --list - docker images
     echo    -rc, --remove-containers - remove all containers
     echo    -ri, --remove-images - remove all images forced
     echo    -c, --console - console in container
     exit 1
+fi
+if [ $STOP ]; then
+    echo --- Stop container
+    docker stop $(docker ps -q)
+fi
+if [ $REMOVE ]; then
+    echo --- Remove containers
+    docker rm $(docker images -q)
+fi
+if [ $REMOVEIMAGES ]; then
+    echo --- Remove images forced
+    docker rmi -f $(docker images -q)
 fi
 if [ $BUILD ]; then
     if [ $VERSION ]; then
@@ -66,6 +84,16 @@ if [ $BUILD ]; then
     echo --- Build image
     docker build -t edeqa/waytous-server:$VERSION .. -f ./Dockerfile.$VERSION
 fi
+if [ $PUSH ]; then
+    if [ $VERSION ]; then
+        echo --- Set version: $VERSION
+    else
+        echo --- Define version: -v=VERSION
+        exit 1;
+    fi
+    echo --- Publish image in repository
+    docker push edeqa/waytous-server:$VERSION
+fi
 if [ $RUN ]; then
     if [ $VERSION ]; then
         echo --- Set version: $VERSION
@@ -74,7 +102,18 @@ if [ $RUN ]; then
         exit 1;
     fi
     echo --- Run container
-    docker run -d -p 8080:8080 -p 8100:8100 -p 8101:8101 -p 8200:8200 -p 8201:8201 -p 8443:8443 -p 8989:8989 edeqa/waytous-server:$VERSION
+    docker run -p 8080:8080 -p 8100:8100 -p 8101:8101 -p 8200:8200 -p 8201:8201 -p 8443:8443 -p 8989:8989 edeqa/waytous-server:$VERSION
+#    docker run -p 8080:8080 -p 8100:8100 -p 8101:8101 -p 8200:8200 -p 8201:8201 -p 8443:8443 -p 8989:8989 edeqa/waytous-server:1.50
+fi
+if [ $DETACHED ]; then
+    if [ $VERSION ]; then
+        echo --- Set version: $VERSION
+    else
+        echo --- Define version: -v=VERSION
+        exit 1;
+    fi
+    echo --- Run container detached, see waytous.log
+    docker run -d -p 8080:8080 -p 8100:8100 -p 8101:8101 -p 8200:8200 -p 8201:8201 -p 8443:8443 -p 8989:8989 edeqa/waytous-server:$VERSION > waytous.log
 #    docker run -p 8080:8080 -p 8100:8100 -p 8101:8101 -p 8200:8200 -p 8201:8201 -p 8443:8443 -p 8989:8989 edeqa/waytous-server:1.50
 fi
 if [ $CONSOLE ]; then
@@ -87,31 +126,9 @@ if [ $CONSOLE ]; then
     echo --- Console in container
     docker run -i -t edeqa/waytous-server:$VERSION /bin/bash
 fi
-if [ $STOP ]; then
-    echo --- Stop container
-    docker stop $(docker ps -q)
-fi
-if [ $REMOVE ]; then
-    echo --- Remove containers
-    docker rm $(docker images -q)
-fi
 if [ $LIST ]; then
     echo --- List images
     docker images
-fi
-if [ $REMOVEIMAGES ]; then
-    echo --- Remove images forced
-    docker rmi -f $(docker images -q)
-fi
-if [ $PUSH ]; then
-    if [ $VERSION ]; then
-        echo --- Set version: $VERSION
-    else
-        echo --- Define version: -v=VERSION
-        exit 1;
-    fi
-    echo --- Publish image in repository
-    docker push edeqa/waytous-server:$VERSION
 fi
 
 
