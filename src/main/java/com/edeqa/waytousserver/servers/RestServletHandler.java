e4559ced19655345486efecad57978e27509a6e7
eduardm <eduardm@pantheon-inc.com>
2018-03-16 17:32:08 -0400

    EQ-17 PageEditHolder - for adding and editing

--- src/main/java/com/edeqa/waytousserver/servers/RestServletHandler.java
+++ src/main/java/com/edeqa/waytousserver/servers/RestServletHandler.java
@@ -1,9 +1,7 @@
 package com.edeqa.waytousserver.servers;
 
+import com.edeqa.edequate.rest.Arguments;
 import com.edeqa.edequate.rest.Files;
-import com.edeqa.edequate.rest.Locales;
-import com.edeqa.edequate.rest.Resource;
-import com.edeqa.helpers.interfaces.Callable1;
 import com.edeqa.waytousserver.helpers.Common;
 import com.edeqa.waytousserver.rest.InitialData;
 import com.edeqa.waytousserver.rest.Join;
@@ -16,8 +14,6 @@ import java.util.Comparator;
 
 import javax.servlet.ServletException;
 
-import static com.edeqa.waytous.Constants.OPTIONS;
-
 /**
  * Created 1/19/17.
  */
@@ -28,12 +24,11 @@ public class RestServletHandler extends com.edeqa.edequate.RestServletHandler {
     public RestServletHandler() {
         super();
         useDefault();
-        registerAction(new Resource().setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("content").setActionName("/rest/content"));
-        registerAction(new Resource().setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("resources").setActionName("/rest/resources"));
-        registerAction(new Resource().setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("data").setActionName("/rest/data"));
+
+        Arguments arguments = (Arguments) getSystemBus().getHolder(Arguments.TYPE);
+
         registerAction(new InitialData());
         registerAction(new Join());
-        registerAction(new Locales().setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("resources").setActionName("/rest/locales"));
         registerAction(new TosAgreement());
         registerAction(new Version());
         registerAction(new Files().setFilenameFilter(new FilenameFilter() {
@@ -47,30 +42,13 @@ public class RestServletHandler extends com.edeqa.edequate.RestServletHandler {
                 if("none.mp3".equals(o1.getName())) return -1;
                 return o1.getName().compareToIgnoreCase(o2.getName());
             }
-        }).setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("sounds").setActionName("/rest/sounds"));
-        registerAction(new Files().setFilenameFilter(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.contains("pages-");
-            }
-        }).setFilenameProcess(new Callable1<String, String>() {
-            @Override
-            public String call(String s) {
-                return s.replaceAll("pages-(.*?)\\.json", "$1");
-            }
-        }).setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("data").setActionName("/rest/data/types"));
+        }).setWebDirectory(arguments.getWebRootDirectory()).setChildDirectory("sounds").setActionName("/rest/sounds"));
         registerAction(new Files().setFilenameFilter(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
                 return name.contains("Holder");
             }
-        }).setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("js/main").setActionName("/rest/main"));
-        registerAction(new Files().setFilenameFilter(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.contains("Holder");
-            }
-        }).setWebDirectory(OPTIONS.getWebRootDirectory()).setChildDirectory("js/tracking").setActionName("/rest/tracking"));
+        }).setWebDirectory(arguments.getWebRootDirectory()).setChildDirectory("js/tracking").setActionName("/rest/tracking"));
 
         registerActionsPool();
     }
